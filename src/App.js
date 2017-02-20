058b7b1d7691a5e8b11386082f889cfb44ecf2b3
MK <mk@macbook-pro-de-mk.home>
2017-02-20 16:35:53 +0100

    Cors request implementation

--- src/App.js
+++ src/App.js
@@ -29,9 +29,39 @@ class App extends Component {
 
 		this.checkForMatch(urlDef, this.state.wordInput);
 	}
+	createCORSRequest(method, url) {
+	  var xhr = new XMLHttpRequest();
+	  if ("withCredentials" in xhr) {
+
+	    // Check if the XMLHttpRequest object has a "withCredentials" property.
+	    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+	    xhr.open(method, url, true);
+
+	  } else if (typeof XDomainRequest != "undefined") {
+
+	    // Otherwise, check if XDomainRequest.
+	    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+	    xhr = new XDomainRequest();
+	    xhr.open(method, url);
+
+	  } else {
+
+	    // Otherwise, CORS is not supported by the browser.
+	    xhr = null;
+
+	  }
+	  return xhr;
+	}	
 	checkForMatch(urlDef, searchRequestFull){
 		// Requête sur le dictionnaire des synonymes :
-		var reqDef = new XMLHttpRequest();
+
+		// var reqDef = new XMLHttpRequest();
+		var reqDef = this.createCORSRequest('GET', urlDef);
+		if (!reqDef) {
+		  throw new Error('CORS not supported');
+		}		
+		reqDef.withCredentials = true;
+
 		reqDef.overrideMimeType('text/xml');
 		reqDef.open('GET', urlDef, true); // Si true : asynchrone
 		reqDef.send(null);
