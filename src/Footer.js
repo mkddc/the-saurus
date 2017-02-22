2c0f744a99dd78e31e6321dc77a7d870688e94e8
MK <mk@macbook-pro-de-mk.home>
2017-02-22 13:36:39 +0100

    footer css issue fixed

--- src/Footer.js
+++ src/Footer.js
@@ -17,7 +17,7 @@ export class Footer extends Component {
 	render(){
 		return(
 			<footer id="myFooter" 
-					style={ this.state.isFirst === true ? { position: 'fixed', bottom: 0 } : {} }>
+					style={ this.state.isFirst === true ? { position: 'fixed', bottom: 0 } : { display: 'none'} }>
 			    <p>
 			      the-saurus uses the <a target="_blank" href="http://www.dictionaryapi.com/">Dictionary API</a> from <a target="_blank" href="https://www.merriam-webster.com/">Merriam-Webster Inc. <img id="logoMW" src="mw-logo-light-background-50x50.png" alt="logo Merriam-Webster Inc." /></a>
 			    </p>
